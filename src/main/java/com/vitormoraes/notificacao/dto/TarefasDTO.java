e2aca1d9398e3ab653cc7e5c661f29a0ec2c9aba
Vitor Moraes <vitor.tm@gmail.com>
2025-11-28 20:27:05 -0300

    erro ao executar aplicacao

---
+++ src/main/java/com/vitormoraes/notificacao/dto/TarefasDTO.java
@@ -0,0 +1,32 @@
+package com.vitormoraes.notificacao.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.vitormoraes.notificacao.business.enums.StatusNotificacaoEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class TarefasDTO {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
